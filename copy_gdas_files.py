f620265a1d26960b470be99b5d10fc353bfdb93e
Allan Lee <alee31@pfe24.nas.nasa.gov>
2023-01-03 16:20:05 -0800

    Updated dates, directory for GDAS data, and added comments.

--- copy_gdas_files.py
+++ copy_gdas_files.py
@@ -4,12 +4,12 @@ from pathlib import Path
 import subprocess
 
 start_date = datetime(2022, 9, 4, 0)
-end_date = datetime(2022, 9, 28, 18)
+end_date = datetime(2022, 9, 30, 18)
 freq = timedelta(hours=6)
 
-gdas_dir = lambda date: f"/nobackupp19/schen4/p7/schen4_nbp1/CPEX_AW/CPEX_CV/data/GDAS_CPEX/{date:%Y%m%d}/"
+gdas_dir = lambda date: f"../CPEX-CV/GDAS_org/{date:%Y%m%d}/"
 prepbufr_filename = lambda date: f"gdas.t{date:%H}z.prepbufr.nr"
-destination_dir = lambda date: f"../CPEX-CV/GDAS/{date:%Y%m%d}/"
+destination_dir = lambda date: f"../CPEX-CV/GDAS3/{date:%Y%m%d}/"
 dawn_filename = lambda date: f"gdas_dawn.t{date:%H}z.prepbufr.nr"
 dropsonde_filename = lambda date: f"gdas_dropsonde.t{date:%H}z.prepbufr.nr"
 halo_filename = lambda date: f"gdas_halo.t{date:%H}z.prepbufr.nr"
@@ -19,21 +19,27 @@ dawn_halo_filename = lambda date: f"gdas_dawn_halo.t{date:%H}z.prepbufr.nr"
 for date in pd.date_range(start_date, end_date, freq=freq):
     print(date)
     Path(f"{destination_dir(date)}").mkdir(parents=True, exist_ok=True)
+    # GDAS only
     subprocess.run(
         f"cp {gdas_dir(date)}/{prepbufr_filename(date)} {destination_dir(date)}/{prepbufr_filename(date)}",
         shell=True)
+    # GDAS + DAWN
     subprocess.run(
         f"cp {gdas_dir(date)}/{prepbufr_filename(date)} {destination_dir(date)}/{dawn_filename(date)}",
         shell=True)
+    # GDAS + dropsonde
     subprocess.run(
         f"cp {gdas_dir(date)}/{prepbufr_filename(date)} {destination_dir(date)}/{dropsonde_filename(date)}",
         shell=True)
+    # GDAS + HALO
     subprocess.run(
         f"cp {gdas_dir(date)}/{prepbufr_filename(date)} {destination_dir(date)}/{halo_filename(date)}",
         shell=True)
+    # GDAS + DAWN + dropsonde + HALO
     subprocess.run(
         f"cp {gdas_dir(date)}/{prepbufr_filename(date)} {destination_dir(date)}/{dawn_dropsonde_halo_filename(date)}",
         shell=True)
+    # GDAS + DAWN + HALO
     subprocess.run(
         f"cp {gdas_dir(date)}/{prepbufr_filename(date)} {destination_dir(date)}/{dawn_halo_filename(date)}",
         shell=True)
